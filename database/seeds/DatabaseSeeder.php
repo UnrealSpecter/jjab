1fdb154da18d9e71ce8fad1db99e46dc1a76605d
yarno <minejarno@gmail.com>
2018-02-26 20:27:48 +0100

    voyager shizzle

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Contact;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,5 +14,15 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(VoyagerDatabaseSeeder::class);
         $this->call(VoyagerDummyDatabaseSeeder::class);
+
+
+        Contact::create([
+            'PhoneNumber'        => '31622654017',
+            'Email'        => 'renda.wesley@gmail.com',
+            'StreetName'          => 'Baander',
+            'HouseNumber'             => '5',
+            'PostalCode'            => '7811HH',
+            'Residence'             => 'Emmen',
+        ])->save();
     }
 }
