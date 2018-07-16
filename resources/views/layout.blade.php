5200b16eeb1cf78b16241801115c5359df12c506
vagrant <vagrant@homestead>
2018-07-16 14:27:55 +0000

    added laravel pdf upload

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,17 @@
     </head>
     <body>
         <div style="height: 100%; width: 100%; padding: 0; margin: 0;" data-enhance="false" data-role="page">
-            <a class="anton col-4 col-sm-2 col-md-3 col-lg-2 col-xl-1 d-flex align-items-center justify-content-center anbi home-anbi" href="newsletter_01.pdf" target="_blank">ANBI</a>
+            @foreach(json_decode($contact->anbi) as $file)
+            <a class="anton col-4 col-sm-2 col-md-3 col-lg-2 col-xl-1 d-flex align-items-center justify-content-center anbi home-anbi" href="{{ url('storage') }}/{{ $file->download_link }}" download>ANBI</a>
+            @endforeach
             <div class="row" id="fullpage" style="height: 100vh; width: 100%; margin: 0; padding: 0;">
                 @yield('content')
             </div>
         </div>
+        <script>
+            var contact = @json($contact->anbi);
+            console.log(contact);
+        </script>
         @include('scroll-indicators')
         @include('scripts')
         <div id="outdated"></div>
