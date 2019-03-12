d25a886d1cb61b503ebef705827278daa85ca566
vagrant <vagrant@homestead>
2019-03-12 10:12:58 +0000

    more alt fixes

--- resources/views/scroll-indicators.blade.php
+++ resources/views/scroll-indicators.blade.php
@@ -2,20 +2,20 @@
 <div class="col-12 d-flex flex-column align-items-center justify-content-center scroll-indicator home d-none">
     <a href="#waar-helpen-we-mee" class="d-flex flex-column align-items-center purple">
         <span class="uppercase anton scroll-text animated fadeIn no-drag">Iets voor mij?</span>
-        <img src="{{ asset('/images/arrows/arrow-big-purple.svg') }}" class=" d-sm-none d-md-block scroll-arrow animated fadeInUp no-drag">
+        <img alt="scroll naar beneden knop" src="{{ asset('/images/arrows/arrow-big-purple.svg') }}" class=" d-sm-none d-md-block scroll-arrow animated fadeInUp no-drag">
     </a>
 </div>
 
 <div class="col-12 d-flex flex-column align-items-center justify-content-center scroll-indicator  scroll-quotes d-none">
     <a href="#wie-zijn-wij" class="d-flex flex-column align-items-center orange">
         <span class="uppercase anton scroll-text animated fadeIn no-drag">En ik wil hulp</span>
-        <img src="{{ asset('/images/arrows/arrow-big-orange.svg') }}" class="img-fluid d-sm-none d-md-block scroll-arrow animated fadeInUp no-drag">
+        <img alt="scroll naar beneden knop" src="{{ asset('/images/arrows/arrow-big-orange.svg') }}" class="img-fluid d-sm-none d-md-block scroll-arrow animated fadeInUp no-drag">
     </a>
 </div>
 
 <div class="col-12 d-flex flex-column align-items-center justify-content-center scroll-indicator who-are-we purple d-none">
     <a href="#contact" class="d-flex flex-column align-items-center purple">
         <span class="uppercase anton scroll-text animated fadeIn no-drag">Hoe neem ik contact op?</span>
-        <img src="{{ asset('/images/arrows/arrow-big-purple.svg') }}" class="img-fluid d-sm-none d-md-block scroll-arrow animated fadeInUp no-drag">
+        <img alt="scroll naar beneden knop" src="{{ asset('/images/arrows/arrow-big-purple.svg') }}" class="img-fluid d-sm-none d-md-block scroll-arrow animated fadeInUp no-drag">
     </a>
 </div>
