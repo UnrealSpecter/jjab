df0b5aa9a2a630c45f9e633babf9da3c575181ac
UnrealSpecter <tomi_emmen@hotmail.com>
2019-11-01 15:25:31 +0100

    readded google analytics

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -8,12 +8,9 @@
     <script src="{{ asset('/scripts/scroll.js')}}"></script>
     <script src="{{ asset('scripts/emergence.js') }}"></script>
     <script src="{{ asset('scripts/menu.js') }}"></script>
-    <!-- <script src="{{ asset('/scripts/logo-animation.js') }}"></script> -->
-    <!-- <script src="//cdn.jsdelivr.net/npm/eruda"></script>
-    <script>eruda.init();</script> -->
 
     <!-- ANALYTICS -->
-    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118817546-1"></script> -->
-    <!-- <script src="{{ asset('plugins/google-analytics/script.js') }}"></script> -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118817546-1"></script>
+    <script src="{{ asset('plugins/google-analytics/script.js') }}"></script>
 
     @yield('additional-scripts')
