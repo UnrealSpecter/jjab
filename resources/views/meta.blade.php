3e52db388a8aab3e7e226f3e8b219f9ad28ce460
UnrealSpecter <tomi_emmen@hotmail.com>
2020-09-01 12:19:16 +0200

    meta fix

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <meta name="title" content="Gratis juridisch advies | Jongeren tussen de 12 en 27 jaar |  Jeugd Juridisch Advies Bureau ( JJAB )">
 <meta name="description" content="JJAB is gericht op het gratis helpen van jongeren in omgeving Emmen met juridische vragen in de breedste zin van het woord.">
